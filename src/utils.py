f19762c92e67796bb20230619d2351ce8c432708
Pierre Chuzeville <pierre.chuzeville58@gmail.com>
2025-02-14 20:25:15 +0100

    predict pipeline

--- src/utils.py
+++ src/utils.py
@@ -46,3 +46,9 @@ def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     except Exception as e:
         raise Custom_Exception(e,sys)
                          
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise Custom_Exception(e,sys)
